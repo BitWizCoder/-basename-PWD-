a0e640a148b858d4aeceac8b6338e7452576d38f
Muhammad Noman <mhmdnoman01@gmail.com>
2025-01-02 00:33:25 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        Primary: {
+          500: '#E8491D',
+        },
+        Secondary: {
+          500: '#35424A',
+        },
+      },
+      fontFamily: {
+        'roboto': ["Roboto", "serif"],
+      },
+    },
+  },
+  plugins: [],
+}
