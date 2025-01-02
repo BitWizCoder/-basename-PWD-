e2d4a71e9ed21ea95c0961d82c22722caac172de
Muhammad Noman <mhmdnoman01@gmail.com>
2025-01-02 21:52:26 +0600

    finished homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       colors: {
         Primary: {
-          500: '#E8491D',
+          500: '#259CE2',
         },
         Secondary: {
           500: '#35424A',
@@ -13,7 +13,7 @@ module.exports = {
       },
       fontFamily: {
         'roboto': ["Roboto", "serif"],
-      },
+      }
     },
   },
   plugins: [],
